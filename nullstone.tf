7ea1205970e8ad164465f90662f264fefd50eb63
Brad Sickles <brad.sickles@gmail.com>
2022-05-06 15:43:03 -0400

    Fix flask cookies apply

--- nullstone.tf
+++ nullstone.tf
@@ -9,7 +9,6 @@ terraform {
 data "ns_workspace" "this" {}
 
 locals {
-  tags          = data.ns_workspace.this.tags
-  block_name    = data.ns_workspace.this.block_name
-  resource_name = "${data.ns_workspace.this.block_ref}-${random_string.resource_suffix.result}"
+  tags       = data.ns_workspace.this.tags
+  block_name = data.ns_workspace.this.block_name
 }
